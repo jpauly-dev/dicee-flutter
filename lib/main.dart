32a815ead3b506e804b9fecddff1db9e560ec792
Jonathan Pauly <klang337@gmail.com>
2023-11-10 23:35:09 +0100

    Fix build + ajout linter + update gitignore

--- lib/main.dart
+++ lib/main.dart
@@ -6,16 +6,18 @@ void main() {
       home: Scaffold(
         backgroundColor: Colors.red,
         appBar: AppBar(
-          title: Text('Dicee'),
+          title: const Text('Dicee'),
           backgroundColor: Colors.red,
         ),
-        body: DicePage(),
+        body: const DicePage(),
       ),
     ),
   );
 }
 
 class DicePage extends StatelessWidget {
+  const DicePage({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Container();
