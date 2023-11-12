336004433c1eed8131e2644df8e370e60124ac57
Jonathan Pauly <klang337@gmail.com>
2023-11-12 09:42:19 +0100

    62 - Challenge solution

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -15,11 +17,66 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
   const DicePage({super.key});
 
   @override
+  State<DicePage> createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  var leftDiceNumber = 1;
+  var rightDiceNumber = 1;
+
+  int randomFace() {
+    return Random().nextInt(6) + 1;
+  }
+
+  void changeDiceFace() {
+    setState(() {
+      leftDiceNumber = randomFace();
+      rightDiceNumber = randomFace();
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Row(
+        children: [
+          Dice(
+            faceNumber: leftDiceNumber,
+            onPressed: () {
+              changeDiceFace();
+              print("left dice clicked");
+            },
+          ),
+          Dice(
+            faceNumber: rightDiceNumber,
+            onPressed: () {
+              changeDiceFace();
+              print("right dice clicked");
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class Dice extends StatelessWidget {
+  final void Function() onPressed;
+  final int faceNumber;
+
+  const Dice({super.key, required this.onPressed, required this.faceNumber});
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: MaterialButton(
+        onPressed: onPressed,
+        child: Image.asset('images/dice$faceNumber.png'),
+      ),
+    );
   }
 }
